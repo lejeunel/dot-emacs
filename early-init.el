8f04062a4f9557af5f34b975ec008764474af884
lejeunel <olol85@gmail.com>
2026-09-06 08:18:09 +0200

    disable custom bg in early-init
    This does not play well on first start...

--- early-init.el
+++ early-init.el
@@ -1,8 +1,2 @@
 ;;; early-init.el -*- lexical-binding: t; -*-
 (setq package-enable-at-startup nil)
-
-;;; disable white screen flash on GUI startup
-(setq default-frame-alist '(
-  (background-color . "#000000")
-  (ns-appearance . dark)
-  (ns-transparent-titlebar . t)))
